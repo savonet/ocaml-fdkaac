9bb5b38f4827676036129cc0d95fe1237a90dc74
Romain Beauxis <toots@rastageeks.org>
2020-03-28 09:51:27 -0500

    Add travis & github actions.

---
+++ .travis-ci.sh
@@ -0,0 +1,14 @@
+# Hacking the build into Travis-CI "C" environment
+# See http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
+
+export OPAMYES=1
+opam init
+if [ -n "${OPAM_SWITCH}" ]; then
+    opam switch ${OPAM_SWITCH}
+fi
+eval `opam config env`
+opam install -q -y dune depext
+opam depext -y -i fdkaa
+
+# compile
+dune build
